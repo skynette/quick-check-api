a32c57199943c09605fa82280d96f98e54221538
skynette <cutejosh2@gmail.com>
2025-03-23 17:59:51 +0100

    Implement app initialization in NewsConfig to start the scheduler

--- news/apps.py
+++ news/apps.py
@@ -7,13 +7,13 @@ class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news'
 
-    # def ready(self):
-    #     """
-    #     Initialize the app, including starting the scheduler
-    #     """
-    #     from . import scheduler
-    #     try:
-    #         scheduler.start()
-    #         logger.info("APScheduler has been started")
-    #     except Exception as e:
-    #         logger.error(f"Failed to start APScheduler: {str(e)}", exc_info=True)
+    def ready(self):
+        """
+        Initialize the app, including starting the scheduler
+        """
+        from . import scheduler
+        try:
+            scheduler.start()
+            logger.info("APScheduler has been started")
+        except Exception as e:
+            logger.error(f"Failed to start APScheduler: {str(e)}", exc_info=True)
